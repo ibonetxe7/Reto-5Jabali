7d6417d32584ce9f243c48141bb0271df24748bb
Gahona <gahonaunax@gmail.com>
2026-04-28 13:02:59 +0200

    Actu de pagina
    -El botón de instagram funciona
    -Cuando rellenas en formulario se envian los datos a una base de datos online.

---
+++ fase9reto4/config.py
@@ -0,0 +1,10 @@
+# =============================================
+#  JABALÍ NUTRIENT — Configuración MySQL
+# =============================================
+
+DB_CONFIG = {
+    'host':     'localhost',
+    'user':     'root',
+    'password': 'Passw0rd',
+    'database': 'JABALÍ'
+}
