f5af53cfeeed239b2211c408a750cee31dd2a484
damibonye <damibonye@gmail.com>
2026-05-07 14:08:12 +0200

    Update config.py

--- fase9reto4/config.py
+++ fase9reto4/config.py
@@ -2,12 +2,12 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
+# Configuración de la base de datos donde todo viene del .env por eso no se ve las contraseñas.
 DB_CONFIG = {
     'host':     os.getenv('DB_HOST'),
     'user':     os.getenv('DB_USER'),
     'password': os.getenv('DB_PASSWORD'),
     'database': os.getenv('DB_NAME'),
 }
-
+# Token de Hugging Face para acceder a los modelos de IA
 HF_TOKEN = os.getenv('HF_TOKEN')
